b6e6f95a5f5a53d304b3f66b9152df62726811c7
tsinglee <tsinglee2009@aliyun.com>
2022-09-24 16:47:43 +0800

    deploy 's2/e2/p1'  ref: '673acad 添加遮罩提示'

--- learn-front-end/s2/e2/p1/js/platformLauncher.js
+++ learn-front-end/s2/e2/p1/js/platformLauncher.js
@@ -9,14 +9,51 @@ function isMobile() {
 
 //    判断后的操作
 var curIsMobile = location.href.match('m.index.html') != null;
-console.log('-------------------- curIsMobile : ' + curIsMobile)
 
 if (isMobile() != curIsMobile) {
-    // location.href = curIsMobile ? './index.html' : './m.index.html';
-
-    console.log('start replace, cur : ' + curIsMobile + ' href=' + location.href + ', change to ' + (curIsMobile ? './index.html' : './m.index.html'));
-
+    // 显示对应平台页面
     location.replace(curIsMobile ? './index.html' : './m.index.html')
+} else {
+    // 启动时全屏遮罩提示
+    window.addEventListener('load', attachLaucherWelcomeElement);
+}
 
-    console.log('complete replace, href=' + location.href)
+// 附加遮罩提示
+function attachLaucherWelcomeElement() {
+    
+    // create element
+    var eleMask = document.createElement('div')
+    eleMask.className = 'launcher-mask-tips'
+    eleMask.style.position = 'fixed';
+    eleMask.style.top = '0';
+    eleMask.style.left = '0';
+    eleMask.style.zIndex = '100';
+    eleMask.style.width = '100vw';
+    eleMask.style.height = '100vh';
+    eleMask.style.backgroundColor = 'rgba(0,0,0,.5)';
+    eleMask.style.display = 'flex';
+    eleMask.style.justifyContent = 'space-around';
+    eleMask.style.alignItems = 'center';
+    eleMask.style.textAlign = 'center';
+
+    var eleSpan = document.createElement('span')
+    eleSpan.style.color = '#fff';
+    eleSpan.style.fontSize = '5vw';
+    eleSpan.style.textShadow = '4px 4px 4px #C00';
+    eleSpan.innerHTML = curIsMobile ?
+                        '自适应平台功能<br>已自动为您切换成了<b>移动端页面</b>' :
+                        '自适应平台功能<br>已自动为您切换成了<b>电脑端页面</b>' ;
+
+    eleMask.appendChild(eleSpan)
+    document.body.appendChild(eleMask)
+
+    // add event
+    eleMask.addEventListener('mouseup', deattachLaucherWelcomElement);
+}
+
+// 移除遮罩提示
+function deattachLaucherWelcomElement() {
+
+    eleMask = document.querySelector('.launcher-mask-tips')
+    document.body.removeChild(eleMask)
 }
